715e505d3d5bbd93e40381dd34831c0903b30db4
김민서 <75977640+cukminseo@users.noreply.github.com>
2023-08-20 12:45:29 +0900

    [FEAT]Add BottomNav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Favorites from "./pages/Favorites/Favorites.jsx";
+import Mainpage from "./pages/Mainpage/Mainpage.jsx";
+import Mypage from "./pages/Mypage/Mypage.jsx";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/Favorites" element={<Favorites />} />
+                <Route path="/Mainpage" element={<Mainpage />} />
+                <Route path="/Mypage" element={<Mypage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
